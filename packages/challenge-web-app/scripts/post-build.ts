1ce3e21c3f6a32994c0292869cb256613bbcac6b
OrangeSoda <951040628@qq.com>
2026-01-06 22:20:58 +0800

    feat: 添加 Docker 支持和更新相关配置

--- packages/challenge-web-app/scripts/post-build.ts
+++ packages/challenge-web-app/scripts/post-build.ts
@@ -1,8 +1,11 @@
 import fs from 'fs/promises';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const root = path.resolve('.output/server');
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const __dirnameInjectCode = `
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
