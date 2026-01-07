ca303672171aebb216d19261f248b661dcb443cb
OrangeSoda <951040628@qq.com>
2026-01-07 16:56:16 +0800

    feat: 添加 Prisma 种子脚本，更新 Dockerfile 和数据库配置

---
+++ packages/challenge-web-app/scripts/prisma-seed.ts
@@ -0,0 +1,50 @@
+import prisma from '@challenge/database';
+import { hashPassword } from '../server/utils/password';
+
+async function main() {
+   await prisma.$connect();
+
+   const account = 'stillsoda';
+   const password = 'stillsoda123';
+   const email = 'stillsoda123@admin.com';
+   const passwordHash = await hashPassword(password);
+
+   const existingUser = await prisma.user.findUnique({
+      where: { name: account },
+   });
+
+   if (existingUser) {
+      console.log(
+         `ℹ️ Admin user "${account}" already exists. Skipping seeding.`
+      );
+      return;
+   }
+
+   await prisma.user.upsert({
+      where: { name: 'stillsoda' },
+      update: {},
+      create: {
+         name: account,
+         displayName: '超级管理员',
+         auths: {
+            create: {
+               provider: 'EMAIL',
+               password: passwordHash,
+               providerId: email,
+            },
+         },
+      },
+   });
+
+   console.log(
+      `✅ Seeded admin user:\n\n  - Account: ${account}\n  - Email: ${email}\n  - Password: ${password}\n`
+   );
+}
+
+main()
+   .catch((e) => {
+      console.error('❌ Some Error happened when seeding the database.\n', e);
+   })
+   .finally(async () => {
+      await prisma.$disconnect();
+   });
--- packages/database/index.ts
+++ packages/database/index.ts
@@ -1,5 +1,5 @@
-import prisma from './prisma/lib/prisma';
+import prisma from './prisma/lib/prisma.ts';
 
-export * from './prisma/name-map/_output';
+export * from './prisma/name-map/_output.ts';
 
 export default prisma;
